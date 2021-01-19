259f33eef37e7ccc4109b44004a418bef31a9957
Andres Riveros D <andrete@gmail.com>
2021-01-19 15:46:17 -0400

    loopstudios landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Header from "./components/Header";
+import Showcase from "./components/Showcase";
+import Interactive from "./components/Interactive";
+import Creations from "./components/Creations";
+import CreationsDesktop from "./components/CreationsDesktop";
+import Footer from "./components/Footer";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Showcase />
+      <Interactive />
+      <Creations />
+      <CreationsDesktop />
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
